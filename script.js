a3acba0bd624af11aae9da78d60f2e99daa9caea
Michelle Lefebvre <mmldesigns007@gmail.com>
2026-03-18 18:39:27 -0400

    add dark mode with icons add modal to project

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function toggleDarkMode() {
+  var element = document.body
+  element.classList.toggle("dark-mode")
+}
 
+// Modal
+const modal = document.getElementsByClassName("prj-modal")
+const openBtn = document.getElementById("openBtn")
+const closeBtn = document.getElementById("closeBtn")
+
+// Open as a modal
+openBtn.addEventListener("click", () => {
+  modal.showModal()
+})
+
+// Close modal
+closeBtn.addEventListener("click", () => {
+  modal.close()
+})
+
+// Optional: Close when clicking the backdrop (outside the modal box)
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.close()
+  }
+})
